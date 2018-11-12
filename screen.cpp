96dbcaa0c4fb9f712ca1eaf5642a3ccdc3161931
MateuszSumiec <msumiec@gmail.com>
2018-11-12 20:21:01 +0100

    adding a Screen class

---
+++ screen.cpp
@@ -0,0 +1,109 @@
+#include "screen.h"
+#include <string.h>
+
+namespace srn{
+
+Screen::Screen():
+    m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer(NULL) {
+}
+
+
+bool Screen::init(){
+    checkForInitError();
+
+    m_window = SDL_CreateWindow("Particle Fire Explosion",
+                        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    checkForWindowError();
+
+    m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
+    m_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    checkForRendererError();
+    checkForTextureError();
+
+    m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    memset(m_buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+    return true;
+}
+
+
+bool Screen::checkForInitError(){
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+        return false;
+    }
+}
+
+
+bool Screen::checkForWindowError(){
+    if(m_window == NULL){
+        SDL_Quit();
+        return false;
+    }
+}
+
+
+bool Screen::checkForRendererError(){
+    if(m_renderer == NULL){
+        SDL_DestroyWindow(m_window);
+        SDL_Quit();
+        return false;
+    }
+}
+
+
+bool Screen::checkForTextureError(){
+    if(m_texture == NULL){
+            SDL_DestroyRenderer(m_renderer);
+            SDL_DestroyWindow(m_window);
+            SDL_Quit();
+            return false;
+        }
+}
+
+
+void Screen::update(){
+    SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderClear(m_renderer);
+    SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+    SDL_RenderPresent(m_renderer);
+}
+
+void Screen::setPixel(int x, int y, Uint32 red, Uint32 green, Uint32 blue){
+    Uint32 color = 0;
+
+    color += red;
+    color <<= 8;
+    color += green;
+    color <<= 8;
+    color += blue;
+    color <<= 8;
+    color += 0xFF;
+
+    m_buffer[(y * SCREEN_WIDTH) + x] = color;
+}
+
+
+bool Screen::procesEvents(){
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event)){
+        if(event.type == SDL_QUIT){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void Screen::close(){
+    delete [] m_buffer;
+    SDL_DestroyRenderer(m_renderer);
+    SDL_DestroyTexture(m_texture);
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+}
+
+}
